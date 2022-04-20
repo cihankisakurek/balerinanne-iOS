ed042e2e3c1b4ab66a55e4dd66fcfdf6817b9b27
Cihan Kisakurek <c.kisakuerek@appdev.de>
2022-04-20 21:16:35 +0200

    Initial Commit

---
+++ Balerinanne/BalerinanneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalerinanneApp.swift
+//  Balerinanne
+//
+//  Created by Cihan Kisakurek on 20.04.22.
+//
+
+import SwiftUI
+
+@main
+struct BalerinanneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
